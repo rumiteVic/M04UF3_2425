82633b7460fb552dd9b65998053c080473fff3d9
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-21 00:09:09 +0100

    Update App.js

--- rollmania/src/App.js
+++ rollmania/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Die from './Die.js';
 import Title from './Title.js';
-
+import Section from './Section.js';
 function App() {
   return (
     <div className="rollmania">
-		<Title />
-		<main className="App">
-		<Die />
-		<Die />
-		<Die />
-    </main>
-	</div>
+        <Title />
+        <main className="App">
+        <div className="fila1"><Die />
+        <Die />
+        <Die />
+        </div>
+<div className="fila2">
+        <Section />
+        <Section />
+        <Section />
+        </div>
+        </main>
+    </div>
  );
 }
 
