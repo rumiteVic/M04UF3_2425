616399abb67cd505b88405b9e4eae08e6e3414b7
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-24 20:45:45 +0100

    avance

--- rollmania/src/RollButton.js
+++ rollmania/src/RollButton.js
@@ -1,27 +1,29 @@
 import './RollButton.css';
 import { useState, useEffect } from 'react';
 
-function RollButton(props) {	
-	
-	const [count, setCount] = useState(0);
-	
-	useEffect(() => {
-		if(count > 0){
+function RollButton(props) {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if(count > 0){
     const intervalId = setInterval(() => {
       setCount(c => c - 1);
     }, 1000);
     return () => clearInterval(intervalId);
-		}
+        }
   }, [count]);
 
 function click (){
-	props.roll_func();
-	setCount(5);
+if(count === 0){
+    setCount(5);
+    props.roll_func();
 }
-	
+}
+
   return (
     <button className="RollButton" onClick={click} >
-		{ count > 0 ? `${count}` : "Rol!!!"}
+    {count > 0? `${count}` : "Roll!!!" }
     </button>
   );
 }
